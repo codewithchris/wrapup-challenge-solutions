291958699d74b8777ecb42579fb69da20fde29a8
Iñaki Narciso <1917005+iomusashi@users.noreply.github.com>
2023-10-24 19:35:39 +0800

    Add call and email functionality using URL schemes tel and mailto

--- Module 04 - Wrap-up Challenge/PhoneBook/PhoneBook/Scenes/EmployeeView/Row/EmployeeCardView.swift
+++ Module 04 - Wrap-up Challenge/PhoneBook/PhoneBook/Scenes/EmployeeView/Row/EmployeeCardView.swift
@@ -29,24 +29,30 @@ struct EmployeeCardView: View {
                     
                     Spacer()
                     
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "phone.circle.fill")
-                            .font(.largeTitle)
-                            .foregroundStyle(.white)
-                            .frame(width: 48, height: 48)
+                    if let phoneUrl = URL(string: "tel:\(employee.phone.replacingOccurrences(of: " ", with: ""))"),
+                       UIApplication.shared.canOpenURL(phoneUrl) {
+                        Button {
+                            UIApplication.shared.open(phoneUrl)
+                        } label: {
+                            Image(systemName: "phone.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .frame(width: 48, height: 48)
+                        }
                     }
                     
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "envelope.circle.fill")
-                            .font(.largeTitle)
-                            .foregroundStyle(.white)
-                            .frame(width: 48, height: 48)
+                    if let emailUrl = URL(string: "mailto:\(employee.email)"),
+                       UIApplication.shared.canOpenURL(emailUrl) {
+                        Button {
+                            UIApplication.shared.open(emailUrl)
+                        } label: {
+                            Image(systemName: "envelope.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .frame(width: 48, height: 48)
+                        }
+                        .padding(.trailing, 16)
                     }
-                    .padding(.trailing, 16)
                 }
             }
         }
